82c6cea2f40eada7902a03447edf099fa2521847
IsaiasGit1 <isaias.espada.git+1@gmail.com>
2023-04-16 19:22:31 +0200

    First commit for feature/odco-0003

---
+++ src/com/iespada/helloworld/Restaurante.java
@@ -0,0 +1,15 @@
+package com.iespada.helloworld;
+
+public class Restaurante {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
