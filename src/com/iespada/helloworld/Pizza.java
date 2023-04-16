0a42db4ca6a6aa9fac4b6728ab6375a460a51119
IsaiasGit1 <isaias.espada.git+1@gmail.com>
2023-04-16 19:02:25 +0200

    First commit for feature/odco-0002

---
+++ src/com/iespada/helloworld/Pizza.java
@@ -0,0 +1,35 @@
+package com.iespada.helloworld;
+
+public final class Pizza {
+
+	private String nombre;
+	private double costoDeProduccion;
+	private int cantidadProducida;
+	private double precioVenta;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getCostoDeProduccion() {
+		return costoDeProduccion;
+	}
+	public void setCostoDeProduccion(double costoDeProduccion) {
+		this.costoDeProduccion = costoDeProduccion;
+	}
+	public int getCantidadProducida() {
+		return cantidadProducida;
+	}
+	public void setCantidadProducida(int cantidadProducida) {
+		this.cantidadProducida = cantidadProducida;
+	}
+	public double getPrecioVenta() {
+		return precioVenta;
+	}
+	public void setPrecioVenta(double precioVenta) {
+		this.precioVenta = precioVenta;
+	}
+	
+}
