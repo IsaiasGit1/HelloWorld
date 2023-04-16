c13d5e3a709ff770b1dad96ec203a11f21a12c1d
IsaiasGit1 <isaias.espada.git+1@gmail.com>
2023-04-16 19:26:02 +0200

    Generacion de la clase restaurante y su App

---
+++ src/com/iespada/helloworld/RestauranteApp.java
@@ -0,0 +1,14 @@
+package com.iespada.helloworld;
+
+public class RestauranteApp {
+
+	public static void main(String[] args) {
+
+		Restaurante restauranteI = new Restaurante();
+		
+		restauranteI.setName("Arzak");
+		restauranteI.setAddress("Alto de Miracruz 1, Donosti");
+
+	}
+
+}
